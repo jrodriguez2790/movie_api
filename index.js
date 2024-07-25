dcf59fac2769defbc6a42920978a2a061cdb613d
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-07-25 16:04:37 -0400

    updates for 2.8
    If applied, this commit will have added endpoints, mongoose DB, and body-parser.

--- index.js
+++ index.js
@@ -1,45 +1,124 @@
 const express = require('express');
-const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Models = require('./models.js');
 const app = express();
 
-// Morgan middleware
-app.use(morgan('dev'));
+mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the public folder
 app.use(express.static('public'));
 
-// Movies data
-const movies = [
-    'Halloween',
-    'House',
-    'Aliens',
-    'The Texas Chain Saw Massacre',
-    'Barbarian',
-    'It Follows',
-    'Hereditary',
-    'Green Room',
-    'Silence of the Lambs',
-    'Pet Sematary'
-];
-
-// GET route for movies
+// Movies endpoints
 app.get('/movies', (req, res) => {
-    res.json({ topMovies: movies });
+  Movies.find()
+    .then(movies => res.json(movies))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.get('/movies/:id', (req, res) => {
+  Movies.findById(req.params.id)
+    .then(movie => res.json(movie))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.post('/movies', (req, res) => {
+  Movies.create(req.body)
+    .then(movie => res.status(201).json(movie))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.put('/movies/:id', (req, res) => {
+  Movies.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+    .then(updatedMovie => res.json(updatedMovie))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.delete('/movies/:id', (req, res) => {
+    console.log('DELETE request received for ID:', req.params.id);
+    Movies.findByIdAndDelete(req.params.id)
+    .then((movie) => {
+      if (!movie) {
+        console.log('Movie not found');
+        return res.status(404).send('Movie not found');
+      }
+      console.log('Movie was deleted:', movie);
+      res.status(200).send('Movie was deleted.');
+    })
+    .catch((err) => {
+        console.error('Error:', err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+// Users endpoints
+app.get('/users', (req, res) => {
+  Users.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.get('/users/:id', (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.post('/users', (req, res) => {
+  Users.create(req.body)
+    .then(user => res.status(201).json(user))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.put('/users/:id', (req, res) => {
+  Users.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+    .then(updatedUser => res.json(updatedUser))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.delete('/users/:id', (req, res) => {
+  Users.findByIdAndDelete(req.params.id)
+    .then(() => res.status(200).send('User was deleted.'))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+// User-Movie (Favorite Movies) endpoints
+app.post('/users/:id/favorites', (req, res) => {
+  Users.findByIdAndUpdate(req.params.id, {
+    $addToSet: { favoriteMovies: req.body.movieId }
+  }, { new: true })
+    .then(updatedUser => res.json(updatedUser))
+    .catch(err => res.status(500).send('Error: ' + err));
+});
+
+app.delete('/users/:id/favorites/:movieId', (req, res) => {
+  Users.findByIdAndUpdate(req.params.id, {
+    $pull: { favoriteMovies: req.params.movieId }
+  }, { new: true })
+    .then(updatedUser => res.json(updatedUser))
+    .catch(err => res.status(500).send('Error: ' + err));
 });
 
 // Default GET route
 app.get('/', (req, res) => {
-    res.send('Welcome to my movie database!');
+  res.send('Welcome to my movie database!');
 });
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 // Start the server
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
