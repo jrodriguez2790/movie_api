b47911515b922624c722627abdd1f7931361ee13
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-08-03 13:20:59 -0400

    updates endpoints adds jwt
    If applied, this commit will have integrated the proper middleware as well as adding JWT authentication to certain endpoints.

--- index.js
+++ index.js
@@ -13,59 +13,59 @@ const Users = Models.User;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const passport = require('passport');
+require('./passport');
+
+app.use(passport.initialize());
+
+let auth = require('./auth')(app);
+
 // Serve static files from the public folder
 app.use(express.static('public'));
 
 // Movies endpoints
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
-    .then(movies => res.json(movies))
-    .catch(err => res.status(500).send('Error: ' + err));
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((error) => {
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-app.get('/movies/:id', (req, res) => {
+app.get('/movies/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.findById(req.params.id)
     .then(movie => res.json(movie))
     .catch(err => res.status(500).send('Error: ' + err));
 });
 
-app.post('/movies', (req, res) => {
+app.post('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.create(req.body)
     .then(movie => res.status(201).json(movie))
     .catch(err => res.status(500).send('Error: ' + err));
 });
 
-app.put('/movies/:id', (req, res) => {
+app.put('/movies/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
     .then(updatedMovie => res.json(updatedMovie))
     .catch(err => res.status(500).send('Error: ' + err));
 });
 
-app.delete('/movies/:id', (req, res) => {
-    console.log('DELETE request received for ID:', req.params.id);
-    Movies.findByIdAndDelete(req.params.id)
-    .then((movie) => {
-      if (!movie) {
-        console.log('Movie not found');
-        return res.status(404).send('Movie not found');
-      }
-      console.log('Movie was deleted:', movie);
-      res.status(200).send('Movie was deleted.');
-    })
-    .catch((err) => {
-        console.error('Error:', err);
-        res.status(500).send('Error: ' + err);
-    });
+app.delete('/movies/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Movies.findByIdAndDelete(req.params.id)
+    .then(() => res.status(200).send('Movie was deleted.'))
+    .catch(err => res.status(500).send('Error: ' + err));
 });
 
 // Users endpoints
-app.get('/users', (req, res) => {
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.find()
     .then(users => res.json(users))
     .catch(err => res.status(500).send('Error: ' + err));
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findById(req.params.id)
     .then(user => res.json(user))
     .catch(err => res.status(500).send('Error: ' + err));
@@ -77,20 +77,41 @@ app.post('/users', (req, res) => {
     .catch(err => res.status(500).send('Error: ' + err));
 });
 
-app.put('/users/:id', (req, res) => {
+app.put('/users/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
     .then(updatedUser => res.json(updatedUser))
     .catch(err => res.status(500).send('Error: ' + err));
 });
 
-app.delete('/users/:id', (req, res) => {
+app.delete('/users/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findByIdAndDelete(req.params.id)
     .then(() => res.status(200).send('User was deleted.'))
     .catch(err => res.status(500).send('Error: ' + err));
 });
 
+app.put('/users/:username', (req, res) => {
+  Users.findOneAndUpdate({ username: req.params.username }, {
+    $set:
+    {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      birthday: req.body.birthday
+    }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+  )
+    .then(updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 // User-Movie (Favorite Movies) endpoints
-app.post('/users/:id/favorites', (req, res) => {
+app.post('/users/:id/favorites', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findByIdAndUpdate(req.params.id, {
     $addToSet: { favoriteMovies: req.body.movieId }
   }, { new: true })
@@ -98,7 +119,7 @@ app.post('/users/:id/favorites', (req, res) => {
     .catch(err => res.status(500).send('Error: ' + err));
 });
 
-app.delete('/users/:id/favorites/:movieId', (req, res) => {
+app.delete('/users/:id/favorites/:movieId', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findByIdAndUpdate(req.params.id, {
     $pull: { favoriteMovies: req.params.movieId }
   }, { new: true })
