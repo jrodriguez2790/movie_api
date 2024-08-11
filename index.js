d69a3ff2fa4f80b1f5826b691f7c5d0543a5d77e
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-08-11 17:59:09 -0400

    updates cors
    If applied, this commit will have updated the CORS so that all domains can access the app.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { check, validationResult } = require('express-validator');
 // mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //online database
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -24,18 +24,7 @@ const passport = require('passport');
 require('./passport');
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.use(passport.initialize());
 
