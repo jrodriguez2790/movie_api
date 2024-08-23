163058907fd797dfc0e7854c5262667baec447b6
jrodriguez2790 <157295161+jrodriguez2790@users.noreply.github.com>
2024-08-23 13:10:23 -0400

    updated movieschema
    If applied, this commit will have updated the movieSchema to make genre an object.

--- models.js
+++ models.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 // Define the Movie schema
 const movieSchema = new Schema({
   title: { type: String, required: true },
   director: { type: String, required: true },
-  genre: { type: String, required: true },
+  genre: {
+    name: { type: String, required: true },
+    description: { type: String, required: true }
+  },
   year: { type: Number, required: true },
   description: { type: String, required: true }
 });
 
+
 // Define the User schema
 const userSchema = new Schema({
+  //_id: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true, unique: true },
